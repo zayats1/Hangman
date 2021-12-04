85449114b73ea4d6e2d43a7a6d8d57cb79365d45
Zaiats Bogdan <40402186+zayats1@users.noreply.github.com>
2021-12-04 19:07:48 +0200

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/bogdandev/exercise/Hangman
-# Build directory: /home/bogdandev/exercise/Hangman/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
